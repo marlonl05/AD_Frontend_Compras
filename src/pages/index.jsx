278e0d30b1dc4382050f9aac2598fbe4e5b01760
Luis Perugachi <luisprg05@gmail.com>
2023-06-23 20:33:12 -0500

    Agregado constantes para estilos de tailwind

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,35 @@
 import { RiCheckboxBlankCircleFill, RiPlayFill } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 
+const circleStyle = 'text-secondary-100 text-base absolute ';
+const buttonStyle = {
+	button:
+		'w-full xl:w-auto flex items-center justify-start text-left gap-4 py-2 px-8 rounded-xl text-xl border border-secondary-100 hover:text-secondary-100 hover:transition-colors',
+	link: 'bg-secondary text-primary p-4 rounded-full box-content',
+};
+
 export const Home = () => {
 	return (
 		<section id='home' className='min-h-[90vh] grid grid-cols-1 xl:grid-cols-8 text-white'>
-			{/* Information */}
 			<div className='md:col-span-5 flex items-center justify-center p-8 xl:p-16'>
-				{/* Information */}
+				{/* Info */}
 				<div className='flex flex-col gap-8'>
 					<h1 className='text-5xl xl:text-7xl font-bold xl:leading-[7.5rem]'>
 						Bienvenido al módulo de
-						<span className='text-primary py-2 px-6 border-8 border-secondary-100 relative inline-block'>
+						<span className='py-2 px-6 border-8 border-secondary-100 relative inline-block'>
 							Compras
-							<RiCheckboxBlankCircleFill className='text-secondary-100 text-base absolute -left-5 -top-5 p-2 bg-primary rounded-full box-content' />
-							<RiCheckboxBlankCircleFill className='text-secondary-100 text-base absolute -right-5 -top-5 p-2 bg-primary rounded-full box-content' />
-							<RiCheckboxBlankCircleFill className='text-secondary-100 text-base absolute -right-5 -bottom-5 p-2 bg-primary rounded-full box-content' />
-							<RiCheckboxBlankCircleFill className='text-secondary-100 text-base absolute -left-5 -bottom-5 p-2 bg-primary rounded-full box-content' />
+							<RiCheckboxBlankCircleFill
+								className={circleStyle + '-left-5 -top-5 p-2 rounded-full box-content'}
+							/>
+							<RiCheckboxBlankCircleFill
+								className={circleStyle + '-right-5 -top-5 p-2 rounded-full box-content'}
+							/>
+							<RiCheckboxBlankCircleFill
+								className={circleStyle + '-right-5 -bottom-5 p-2 marker:rounded-full box-content'}
+							/>
+							<RiCheckboxBlankCircleFill
+								className={circleStyle + '-left-5 -bottom-5 p-2 rounded-full box-content'}
+							/>
 						</span>
 					</h1>
 					<p className='text-gray-300 text-2xl leading-[2.5rem]'>
@@ -23,18 +37,12 @@ export const Home = () => {
 						productos y servicios de manera fácil y rápida.
 					</p>
 					<div className='flex flex-col md:flex-row items-center gap-4'>
-						<Link
-							to='/compras'
-							className='w-full xl:w-auto flex items-center justify-start text-left gap-4 py-2 px-8 rounded-xl text-xl border border-secondary-100 hover:text-secondary-100 hover:transition-colors'
-						>
-							<RiPlayFill className='bg-secondary text-primary p-4 rounded-full box-content' />
+						<Link to='/compras' className={buttonStyle.button}>
+							<RiPlayFill className={buttonStyle.link} />
 							Visitar compras
 						</Link>
-						<Link
-							to='/proveedores'
-							className='w-full xl:w-auto flex items-center justify-start text-left gap-4 py-2 px-8 rounded-xl text-xl border border-secondary-100 hover:text-secondary-100 hover:transition-colors'
-						>
-							<RiPlayFill className='bg-secondary text-primary p-4 rounded-full box-content' />
+						<Link to='/proveedores' className={buttonStyle.button}>
+							<RiPlayFill className={buttonStyle.link} />
 							Visitar provedores
 						</Link>
 					</div>
--- src/pages/shopping/index.jsx
+++ src/pages/shopping/index.jsx
@@ -2,33 +2,30 @@ import { Tab } from '@headlessui/react';
 import { Search } from '../../components/common';
 import { ShoppingList } from '../../components/shopping';
 
+const focusStyle =
+	'relative py-2 pr-4 ui-selected:before:w-3/4 ui-selected:before: ui-selected:before:h-[2px] ui-selected:before:absolute ui-selected:before:bg-secondary-100 ui-selected:before:left-0 ui-selected:before:rounded-full ui-selected:before:-bottom-[1px] outline-none';
+
 export const Shopping = () => {
 	return (
 		<Tab.Group>
 			<header>
 				<Search />
-				<Tab.List className='text-gray-300 flex items-center justify-between md:justify-start md:gap-10 border-b mb-6'>
-					<Tab className='relative py-2 pr-4 ui-selected:before:w-1/2 ui-selected:before:h-[2px] ui-selected:before:absolute ui-selected:before:bg-[#EC7C6A] ui-selected:before:left-0 ui-selected:before:rounded-full ui-selected:before:-bottom-[1px] text-[#EC7C6A] outline-none'>
-						Listado de compras
-					</Tab>
-					<Tab className='relative py-2 pr-4 ui-selected:before:w-1/2 ui-selected:before:h-[2px] ui-selected:before:absolute ui-selected:before:bg-[#EC7C6A] ui-selected:before:left-0 ui-selected:before:rounded-full ui-selected:before:-bottom-[1px] outline-none'>
-						Agregar compra
-					</Tab>
-					<Tab className='py-2 pr-4  outline-none'>Editar compra</Tab>
+				<Tab.List className='text-secondary-100 flex items-center justify-between md:justify-start md:gap-10 border-b mb-6'>
+					<Tab className={focusStyle}>Listado de compras</Tab>
+					<Tab className={focusStyle}>Agregar compra</Tab>
+					<Tab className={focusStyle}>Editar compra</Tab>
 				</Tab.List>
-				{/* <hr className='mb-4' /> */}
 			</header>
-			{/* Title content */}
-			{/* <div className='flex items-center justify-between mb-16'>
-				<h2 className='text-xl text-gray-300'>Listado de compras</h2>
-				<button className='flex items-center gap-5 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg'>
-					<RiArrowDownSLine />
-					Dine in
-				</button>
-			</div> */}
+
 			<Tab.Panels>
 				<Tab.Panel>
 					<ShoppingList />
+				</Tab.Panel>
+				<Tab.Panel>
+					<h1>Agregar compra</h1>
+				</Tab.Panel>
+				<Tab.Panel>
+					<h1>Editar compra</h1>
 				</Tab.Panel>
 			</Tab.Panels>
 		</Tab.Group>
