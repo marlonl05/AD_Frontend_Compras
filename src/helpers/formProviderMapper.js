38d9f2070af03c017e3898eb2f8326e82f1911fb
Luis Perugachi <luisprg05@gmail.com>
2023-06-28 16:54:21 -0500

    Added mapper for providers

---
+++ src/helpers/formProviderMapper.js
@@ -0,0 +1,20 @@
+export const mapNormalToFormProvider = provider => {
+	return {
+		...provider,
+		documento_de_identificacion: provider.documento_identificacion,
+		nombres_completos: provider.nombre,
+		estado_del_proveedor: provider.estado,
+	};
+};
+
+export const mapFormProviderToNormal = provider => {
+	provider.documento_identificacion = provider.documento_de_identificacion;
+	provider.nombre = provider.nombres_completos;
+	provider.estado = provider.estado_del_proveedor;
+
+	delete provider.documento_de_identificacion;
+	delete provider.estado_del_proveedor;
+	delete provider.nombres_completos;
+
+	return provider;
+};
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,3 +2,4 @@ export * from './time';
 export * from './enviroments';
 export * from './responseMapper';
 export * from './stringNormalizer';
+export * from './formProviderMapper';
