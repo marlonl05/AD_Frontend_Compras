d487db3ca6ac1da5c983519f6f17622717514104
Luis Perugachi <luisprg05@gmail.com>
2023-06-30 17:58:49 -0500

    mostrar proveedor seleccionado correcto

--- src/pages/providers/index.jsx
+++ src/pages/providers/index.jsx
@@ -8,7 +8,7 @@ const focusStyle =
 	'relative py-2 pr-4 ui-selected:text-secondary-100 ui-selected:before:w-3/4 ui-selected:before: ui-selected:before:h-[2px] ui-selected:before:absolute ui-selected:before:bg-secondary-100 ui-selected:before:left-0 ui-selected:before:rounded-full ui-selected:before:-bottom-[1px] outline-none';
 
 export const Providers = () => {
-	const { currentProvider, defaultTabIndex, handleTabIndex } = useProviderContext();
+	const { currentProvider, providerList, defaultTabIndex, handleTabIndex } = useProviderContext();
 
 	return (
 		<TableWrapper
@@ -28,7 +28,7 @@ export const Providers = () => {
 						<ProviderList />
 					</Tab.Panel>
 					<Tab.Panel>
-						<ProviderForm />
+						<ProviderForm provider={currentProvider && providerList[+currentProvider]} />
 					</Tab.Panel>
 				</Body>
 			}
