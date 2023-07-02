237830185c6670ed36382ba2732e1680aa65005a
Luis Perugachi <luisprg05@gmail.com>
2023-07-02 13:29:09 -0500

    Controlado excepcion de item nulo en el filtro y agregado compras temporales

--- src/helpers/filterListByPattern.js
+++ src/helpers/filterListByPattern.js
@@ -1,7 +1,7 @@
 export const filterListByPattern = (list, pattern) => {
 	const filteredList = list.filter(item => {
 		const isMatchWithPattern = Object.values(item).some(value =>
-			value.toString().toLowerCase().includes(pattern.toLowerCase())
+			value?.toString().toLowerCase().includes(pattern.toLowerCase())
 		);
 		return isMatchWithPattern;
 	});
