21267a80ee1bf40a0e703c49c42714ef577b7a68
Luis Perugachi <luisprg05@gmail.com>
2023-07-06 09:51:56 -0500

    Componetizado agregar y editar del formulario de compra

---
+++ src/components/shopping/form/AddForm.jsx
@@ -0,0 +1,76 @@
+/* eslint-disable react/prop-types */
+import { providerState, providerTypes } from '../../../constants';
+import { Button, DateField, SelectField } from '../../common';
+
+export const AddForm = ({
+	register,
+	provider,
+	providerList = {},
+	errors = {},
+	setValue,
+	watch,
+	paymentTypes = [],
+	defatultPaymentType,
+}) => {
+	return (
+		<>
+			<SelectField
+				register={register}
+				nameValues={{
+					label: 'Proveedor',
+					input: 'proveedor_id',
+				}}
+				selectList={Object.values(providerList).filter(
+					provider => provider?.estado === providerState.ACTIVO
+				)}
+				defaultValue={providerList[watch('proveedor_id')]}
+				errorMessage={errors.proveedor_id?.message}
+				handleValueOnChange={setValue}
+				propForSelectLabel='nombre'
+				filterPlaceholder='Buscar proveedor...'
+				activeFilter
+				required
+			/>
+
+			<SelectField
+				register={register}
+				nameValues={{
+					label: 'Tipo de pago',
+					input: 'tipo_pago',
+				}}
+				selectList={paymentTypes}
+				defaultValue={defatultPaymentType}
+				errorMessage={errors.tipo_pago?.message}
+				handleValueOnChange={setValue}
+				required
+			/>
+
+			{provider?.tipo_proveedor === providerTypes.CREDITO &&
+				watch('tipo_pago') === providerTypes.CREDITO && (
+					<DateField
+						register={register}
+						nameValues={{
+							label: 'Fecha de vencimiento',
+							input: 'fecha_vencimiento',
+						}}
+						required
+						errorMessage={errors.fecha_vencimiento?.message}
+						defaultValue={watch('fecha_vencimiento')}
+						handleValueOnChange={setValue}
+					/>
+				)}
+
+			<div className='flex flex-col md:flex-row justify-end md:col-span-2 lg2:col-span-1 2xl:col-span-2 gap-2'>
+				<Button type='submit' className='w-auto'>
+					Registrar e imprimir esta factura
+				</Button>
+				<Button type='submit' className='w-auto'>
+					Registrar factura
+				</Button>
+			</div>
+			<span className='text-red-600 text-end md:col-span-2 lg2:col-span-1 2xl:col-span-2 text-sm'>
+				¡Esta acción no se puede deshacer!
+			</span>
+		</>
+	);
+};
---
+++ src/components/shopping/form/EditForm.jsx
@@ -0,0 +1,120 @@
+/* eslint-disable react/prop-types */
+import { RiBillFill, RiCalendarEventFill, RiCurrencyFill, RiUser2Fill } from 'react-icons/ri';
+import { providerTypes, shoppingState } from '../../../constants';
+import { FormFieldUnEditable } from '../../common/form';
+
+const PaymentCheckBox = ({ isCheck, onChange }) => {
+	return (
+		<div className='flex items-start flex-col md:flex-row gap-2 mb-8'>
+			<div className='w-full md:w-2/6'>Estado</div>
+			<div className='flex items-center gap-2'>
+				<label className='relative h-8 w-14 cursor-pointer block'>
+					<input
+						type='checkbox'
+						className='peer sr-only [&:checked_+_span_svg[data-unchecked-icon]]:hidden [&:checked_+_span_svg[data-checked-icon]]:block'
+						defaultChecked={isCheck}
+						onChange={({ target }) => onChange(target.checked)}
+					/>
+					<span className='absolute inset-0 z-10 m-1 inline-flex h-6 w-6 items-center justify-center rounded-full bg-white text-gray-400 transition peer-checked:translate-x-6 peer-checked:text-secondary-100'>
+						<svg
+							data-unchecked-icon
+							xmlns='http://www.w3.org/2000/svg'
+							className='h-4 w-4'
+							viewBox='0 0 20 20'
+							fill='currentColor'
+						>
+							<path
+								fillRule='evenodd'
+								d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
+								clipRule='evenodd'
+							/>
+						</svg>
+
+						<svg
+							data-checked-icon
+							xmlns='http://www.w3.org/2000/svg'
+							className='hidden h-4 w-4'
+							viewBox='0 0 20 20'
+							fill='currentColor'
+						>
+							<path
+								fillRule='evenodd'
+								d='M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z'
+								clipRule='evenodd'
+							/>
+						</svg>
+					</span>
+					<span className='absolute inset-0 rounded-full bg-gray-300 transition peer-checked:bg-secondary-100'></span>
+				</label>
+				<p className='capitalize'>{isCheck ? shoppingState.ACTIVO : shoppingState.INACTIVO}</p>
+			</div>
+		</div>
+	);
+};
+
+export const EditForm = ({ provider, shopping }) => {
+	return (
+		<>
+			<FormFieldUnEditable
+				Icon={RiUser2Fill}
+				nameValues={{
+					label: 'Proveedor',
+					input: provider?.nombre,
+				}}
+				required
+			/>
+			<FormFieldUnEditable
+				Icon={RiBillFill}
+				nameValues={{
+					label: 'Tipo de pago',
+					input: shopping?.tipo_pago,
+				}}
+				required
+			/>
+
+			<FormFieldUnEditable
+				Icon={RiCalendarEventFill}
+				nameValues={{
+					label: 'Fecha de compra',
+					input: shopping?.fecha_factura,
+				}}
+				required
+			/>
+			{provider?.tipo_proveedor === providerTypes.CREDITO &&
+				providerTypes.CREDITO === shopping?.tipo_pago && (
+					<FormFieldUnEditable
+						Icon={RiCalendarEventFill}
+						nameValues={{
+							label: 'Fecha de vencimiento',
+							input: shopping?.fecha_vencimiento,
+						}}
+						required
+					/>
+				)}
+
+			<FormFieldUnEditable
+				Icon={RiCurrencyFill}
+				nameValues={{
+					label: 'Pago total',
+					input: shopping?.total,
+				}}
+				required
+			/>
+
+			<PaymentCheckBox isCheck={shopping.estado === shoppingState.ACTIVO} onChange={console.log} />
+
+			<div className='flex flex-col md:flex-row justify-end md:col-span-2 lg2:col-span-1 2xl:col-span-2 gap-2'>
+				<div className='bg-dark-200 p-8 rounded-xl flex flex-col gap-2 items-center text-gray-300 text-center'>
+					<h3>Detalles del proveedor</h3>
+					<p>Nombre: {provider?.nombre}</p>
+					<p>DNI: {provider?.documento_identificacion}</p>
+					<p>Teléfono: {provider?.telefono}</p>
+					<p>Email: {provider?.email}</p>
+					<p>Dirección: {provider?.direccion}</p>
+					<p>Estado: {provider?.estado}</p>
+					<p>Tipo proveedor: {provider?.tipo_proveedor}</p>
+				</div>
+			</div>
+		</>
+	);
+};
