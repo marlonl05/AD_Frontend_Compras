3c85bc44b7d25048b330f5a785d5418db6473b63
Luis Perugachi <luisprg05@gmail.com>
2023-07-11 10:49:48 -0500

    Agregado icono al boton para descargar los pdf's

--- src/pdf/PdfLink.jsx
+++ src/pdf/PdfLink.jsx
@@ -1,15 +1,23 @@
 /* eslint-disable react/prop-types */
 import { PDFDownloadLink } from '@react-pdf/renderer';
+import { RiBillFill } from 'react-icons/ri';
 
-export const PdfLink = ({ document, fileName, text, getProps }) => (
+export const PdfLink = ({ document, fileName, text, getProps, showIcon = false }) => (
 	<PDFDownloadLink
-		className='font-normal text-start inline-block w-64 md:w-auto truncate text-white px-4 py-2 bg-secondary-100 rounded-md'
+		className='font-normal flex items-center justify-center gap-2 w-64 md:w-full truncate text-white px-4 py-2 bg-secondary-100 rounded-md'
 		document={document}
 		fileName={fileName}
 	>
 		{({ blob, url, loading, error }) => {
 			if (getProps) getProps({ blob, url, loading, error });
-			return loading ? 'Cargando pdf...' : text;
+			return loading ? (
+				'Cargando pdf...'
+			) : (
+				<>
+					{text}
+					{showIcon && <RiBillFill />}
+				</>
+			);
 		}}
 	</PDFDownloadLink>
 );
