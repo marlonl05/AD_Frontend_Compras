16e6fc051241d7ed63156f4fe60d1750072aa771
Luis Perugachi <luisprg05@gmail.com>
2023-07-14 10:58:02 -0500

    Agregado /login ruta

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Home, Providers, Shopping } from '../pages';
 import { PrivateRoutes } from './PrivateRoutes';
+import Loginn from '../pages/Loginn/Loginn';
 
 export const AppRouter = () => {
 	return (
 		<Routes>
 			<Route index element={<Home />} />
+			<Route path='/login' element={<Loginn />} />
 			{/* Private routes */}
 			<Route element={<PrivateRoutes />}>
 				<Route path='/compras' element={<Shopping />} />
--- src/router/PrivateRoutes.jsx
+++ src/router/PrivateRoutes.jsx
@@ -1,12 +1,16 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Layout } from '../layout/Layout';
 import { useAuthContext } from '../hooks/';
 
 export const PrivateRoutes = ({ children }) => {
 	const { logged } = useAuthContext();
+	const { pathname, search } = useLocation();
 
+	const lastPath = pathname + search;
 	const redirectTo = '/login';
+
+	localStorage.setItem('lastPath', lastPath);
 
 	if (!logged) return <Navigate to={redirectTo} replace />;
 
